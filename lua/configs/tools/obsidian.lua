f15e55c74790134e50dd7ab78d249a6b9d523acc
qwislet <linuxuser.zlmug@slmail.me>
2024-05-04 23:04:35 +0400

    chore(obsidian): removed frontmatter function

--- lua/configs/tools/obsidian.lua
+++ lua/configs/tools/obsidian.lua
@@ -20,20 +20,6 @@ local options = {
 
   disable_frontmatter = true,
 
-  note_frontmatter_func = function(note)
-    -- This is equivalent to the default frontmatter function.
-    local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-
-    -- `note.metadata` contains any manually added fields in the frontmatter.
-    -- So here we just make sure those fields are kept in the frontmatter.
-    if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-      for k, v in pairs(note.metadata) do
-        out[k] = v
-      end
-    end
-    return out
-  end,
-
   note_id_func = function(title)
     -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
     -- In this case a note with the title 'My new note' will be given an ID that looks
